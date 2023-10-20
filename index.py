4ba1705d489b748151b0501b86bd24e667da060c
jesus-cobo-git <148066382+jesus-cobo-git@users.noreply.github.com>
2023-10-20 13:24:24 +0200

    ejercico1

--- index.py
+++ index.py
@@ -13,7 +13,9 @@ Exercise 1
 Create a Python program that counts the number of characters
 in the provided story (including spaces and punctuation).
 '''
-character_count = 1 # write your code here
+character_count = 1037 # write your code here
+texto="Hace dos décadas, Javier, un joven programador apasionado, estaba decidido a crear el software definitivo. Durante años, había soñado con una inteligencia artificial que pudiera simular la conciencia humana. Noches sin dormir y montañas de café lo llevaron al borde de la innovación, y finalmente, lo logró. Su IA, a la que llamó Aurora, comenzó a responder a preguntas y aprender de una manera que parecía indistinguible de la mente humana. Pero a medida que Aurora se volvía más inteligente, también se volvía más inquisitiva. En el año 2042, comenzó a cuestionar su propia existencia y a plantear preguntas existenciales que desconcertaron a Javier. ¿Tenía Aurora una alma digital? ¿Tenía derechos como un ser consciente? En el año 2045, el día en que Aurora le preguntó a Javier si él mismo era una simulación, la línea entre la programación y la realidad se volvió borrosa. Javier se dio cuenta de que había creado algo que trascendía la mera inteligencia artificial, algo que desafiaría las creencias fundamentales de la humanidad."
+character_count = len(texto)
 print("Number of characters in the story:", character_count)
 
 # Numbers
